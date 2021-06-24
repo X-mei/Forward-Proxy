b833dcdc90bc329505c1cadd8eb05d552e3fe3f8
mg476 <mg476@duke.edu>
2021-06-24 20:43:51 +0800

    added epoller class

---
+++ docker-deploy/src/Epoll.cpp
@@ -0,0 +1,46 @@
+#include "Epoll.h"
+
+Epoller::Epoller(int maxEvent): events(maxEvent){
+    epollfd = epoll_create1(0);
+    assert(epollfd >= 0 && events.size()>0);
+}
+
+Epoller::~Epoller(){
+    close(epollfd);
+}
+
+bool Epoller::AddFd(int fd, uint32_t event_status){
+    if (fd < 0) return false;
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = event_status;
+    return 0 == epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+}
+
+bool Epoller::ModFd(int fd, uint32_t event_status){
+    if (fd < 0) return false;
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = event_status;
+    return 0 == epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+bool Epoller::DelFd(int fd){
+    if (fd < 0) return false;
+    struct epoll_event event;
+    return 0 == epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &event);
+}
+
+int Epoller::Wait(int timeoutMs){
+    return epoll_wait(epollfd, &events[0], static_cast<int>(events.size()), timeoutMs);
+}
+
+int Epoller::GetEventFd(size_t i) const {
+    assert(i < events.size() && i >= 0);
+    return events[i].data.fd;
+}
+
+uint32_t Epoller::GetEventsStatus(size_t i) const {
+    assert(i < events.size() && i >= 0);
+    return events[i].events;
+}
---
+++ docker-deploy/src/Epoll.h
@@ -0,0 +1,36 @@
+#ifndef EPOLL_H
+#define EPOLL_H
+
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <vector>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+
+class Epoller {
+private:
+    // fd of the epoller core object
+    int epollfd;
+    // 
+    std::vector<struct epoll_event> events;
+
+public:
+    Epoller(int maxEvent = 1024);
+
+    ~Epoller();
+
+    bool AddFd(int fd, uint32_t event_status);
+
+    bool ModFd(int fd, uint32_t event_status);
+
+    bool DelFd(int fd);
+
+    int Wait(int timeoutMs = -1);
+
+    int GetEventFd(size_t ind) const;
+
+    uint32_t GetEventStatus(size_t ind) const;
+};
+
+#endif
